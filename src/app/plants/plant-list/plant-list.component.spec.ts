94952d835719be682ce59702c5cc6494a29489bd
Oscar Buitrago <o.buitragov@uniandes.edu.co>
2023-04-28 21:29:23 -0500

    Punto Número 3 (Prueba del componente)

--- src/app/plants/plant-list/plant-list.component.spec.ts
+++ src/app/plants/plant-list/plant-list.component.spec.ts
@@ -3,6 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PlantListComponent } from './plant-list.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PlantsService } from 'src/app/services/plants.service';
+import { Plant } from 'src/app/models/plant.model';
+import { faker } from '@faker-js/faker';
+import { By } from '@angular/platform-browser';
 
 describe('PlantListComponent', () => {
   let component: PlantListComponent;
@@ -17,10 +20,36 @@ describe('PlantListComponent', () => {
 
     fixture = TestBed.createComponent(PlantListComponent);
     component = fixture.componentInstance;
+
+    for (let i = 0; i < 3; i++) {
+      const plant = new Plant(
+        i + 1,
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.helpers.arrayElement(['Interior', 'Exterior']),
+        faker.datatype.number({ max: 250 }),
+        faker.lorem.sentence(),
+        faker.lorem.sentence()
+      );
+      component.plants.push(plant);
+    }
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have table', () => {
+    expect(fixture.debugElement.queryAll(By.css('table')).length).toEqual(1);
+  });
+
+  it('should have table header', () => {
+    expect(fixture.debugElement.queryAll(By.css('thead')).length).toEqual(1);
+  });
+
+  it('should have 3 columns <tr>', () => {
+    expect(fixture.debugElement.queryAll(By.css('tr')).length).toEqual(3);
   });
 });
